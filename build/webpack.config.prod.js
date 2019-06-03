53a4c3f57d353feb6db2c5a98404f4e66ab5fd14
徐涛焘 <xutaotao1108@outlook.com>
2019-06-03 18:24:47 +0800

    chore:webpack build config change

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,11 +1,29 @@
 'use strict'
-
+const path = require('path');
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  entry: {
+    'ninecatui': './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../package'),
+    publicPath: '/package/',
+    library: 'ninecatui',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  externals: {
+    vue: {
+      root: 'Vue',
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue'
+    }
+  },
   optimization: {
     splitChunks: {
       cacheGroups: {
@@ -31,6 +49,13 @@ module.exports = merge(baseConfig, {
           MiniCssExtractPlugin.loader, 
           'css-loader', 
           'stylus-loader'
+        ]
+      },{
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
         ]
       }
     ]
