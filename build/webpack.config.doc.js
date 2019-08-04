ee2b8d024777b4386aeed10386a81680f4fe6ca8
徐涛焘 <xutaotao1108@outlook.com>
2019-08-04 13:11:56 +0800

    feat:add doc build script

---
+++ build/webpack.config.doc.js
@@ -0,0 +1,66 @@
+'use strict'
+const path = require('path')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  entry: path.resolve(__dirname, '../index.js'),
+  output: {
+    path: path.resolve(__dirname, '../doc'),
+    publicPath: './',
+    library: 'ninecat-doc',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  externals: {
+    vue: {
+      root: 'Vue',
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue'
+    }
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css?$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }, {
+        test: /\.styl(us)?$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ]
+      }, {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'ninecatdoc.min.css'
+    })
+  ]
+})
