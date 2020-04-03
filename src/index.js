c249eb35ba8ee6a8a1813e25d1cca7bece544555
徐涛焘 <xutaotao1108@outlook.com>
2020-04-03 22:36:30 +0800

    refactor: change card style

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import './../assets/font/iconfont.css'
-import nAlert from '../packages/nAlert'
-import nButton from '../packages/nButton'
-import nCard from '../packages/nCard'
-import nBadge from '../packages/nBadge'
-import nLoader from '../packages/nLoader'
-import nIcon from '../packages/nIcon'
-import nMessage from '../packages/nMessage'
-import nButtonGroup from '../packages/nButtonGroup'
-import nInput from '../packages/nInput'
-import nCol from '../packages/nCol'
-import nRow from '../packages/nRow'
-import nToast from '../packages/nToast'
-import nTable from '../packages/nTable'
-import nPagination from '../packages/nPagination'
-import nModal from '../packages/nModal'
-import nProgress from '../packages/nProgress'
-import nDrawer from '../packages/nDrawer'
-import nAvatar from '../packages/nAvatar'
-import nUpload from '../packages/nUpload'
-import nList from '../packages/nList'
-import locale from './locale/index'
+import './../assets/font/iconfont.css';
+import nAlert from '../packages/nAlert';
+import nButton from '../packages/nButton';
+import nCard from '../packages/nCard';
+import nBadge from '../packages/nBadge';
+import nLoader from '../packages/nLoader';
+import nIcon from '../packages/nIcon';
+import nMessage from '../packages/nMessage';
+import nButtonGroup from '../packages/nButtonGroup';
+import nInput from '../packages/nInput';
+import nCol from '../packages/nCol';
+import nRow from '../packages/nRow';
+import nToast from '../packages/nToast';
+import nTable from '../packages/nTable';
+import nPagination from '../packages/nPagination';
+import nModal from '../packages/nModal';
+import nProgress from '../packages/nProgress';
+import nDrawer from '../packages/nDrawer';
+import nAvatar from '../packages/nAvatar';
+import nUpload from '../packages/nUpload';
+import nList from '../packages/nList';
+import locale from './locale/index';
 
 const components = {
   nAlert,
@@ -41,19 +41,19 @@ const components = {
   nUpload,
   nList,
   nCard
-}
+};
 
 const install = function (Vue, opts = {}) {
-  locale.use(opts.locale)
-  locale.i18n(opts.i18n)
+  locale.use(opts.locale);
+  locale.i18n(opts.i18n);
   Object.values(components).forEach(component => {
-    Vue.component(component.name, component)
-  })
-  Vue.prototype.$nMessage = nMessage
-}
+    Vue.component(component.name, component);
+  });
+  Vue.prototype.$nMessage = nMessage;
+};
 
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+  install(window.Vue);
 }
 
 export default {
@@ -61,4 +61,4 @@ export default {
   i18n: locale.i18n,
   install,
   ...components
-}
+};
