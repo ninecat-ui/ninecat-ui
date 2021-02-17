7fc7a66f0edf14cb5b740af82ea6f803c42aecf6
徐涛焘 <xutaotao1108@outlook.com>
2021-02-17 17:01:40 +0800

    chore: change doc build

---
+++ build/webpack.config.doc.js
@@ -0,0 +1,165 @@
+const path = require('path')
+const utils = require('./utils')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/dist/plugin').default;
+const WebpackBar = require('webpackbar');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const babelConfig = {
+  cacheDirectory: true,
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: [
+            'last 2 versions',
+            'Firefox ESR',
+            '> 1%',
+            'ie >= 9',
+            'iOS >= 8',
+            'Android >= 4',
+          ],
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'ninecat',
+        libraryDirectory: '', // default: lib
+        style: true,
+      },
+    ],
+    ['@vue/babel-plugin-jsx', { mergeProps: false }],
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-class-properties',
+  ],
+};
+
+module.exports = () => ({
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: path.resolve(__dirname, '../doc/main.ts'),
+  output: {
+    path: path.resolve(__dirname, '../docs'),
+    publicPath: './',
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: 'vue-loader',
+            options: {
+              compilerOptions: {
+                preserveWhitespace: false
+              }
+            }
+          },
+          {
+            loader: path.resolve(__dirname, './md-loader/index.js')
+          }
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: babelConfig,
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          }
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/,
+        loader: 'file-loader',
+        options: {
+            limit: 10000,
+            name: '[name].[hash:7].[ext]'
+        }
+     },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: utils.assetsPath('media/[name].[hash:7].[ext]')
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts','.js', '.vue', '.json', '.tsx'],
+    alias: {
+      'vue': '@vue/runtime-dom'
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './doc/doc.html',
+      filename: 'index.html',
+      favicon:'./doc/favicon.ico',
+      inject: true,
+    }),
+    new VueLoaderPlugin(),
+    new WebpackBar(),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+})
